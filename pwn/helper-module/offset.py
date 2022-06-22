17d86b3dd13187b347da62cd4f24455b1370c624
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-06-22 20:39:32 +0800

    fix(pwn): offset.py cyclic warning

--- pwn/helper-module/offset.py
+++ pwn/helper-module/offset.py
@@ -1,6 +1,7 @@
 from pwn import *
 from glob import glob
 import os
+from binascii import unhexlify
 
 def offset(io):
     io.sendlineafter(b"> ", cyclic(0x1000))
@@ -11,4 +12,5 @@ def offset(io):
     for file in glob("./core.*"):
         os.remove(file)
 
-    return cyclic_find(core.fault_addr)
+    fault = unhexlify(hex(core.fault_addr)[2:])
+    return cyclic_find(fault[:4])
