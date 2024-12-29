5202f8016b541771072c30c43e688679b3c28fdc
d0ublew <66501624+d0UBleW@users.noreply.github.com>
2024-12-29 19:23:24 +0700

    chore: typo

--- wgmy2024/blockchain/guess-it/EasyChallenge/script/Solve.s.sol
+++ wgmy2024/blockchain/guess-it/EasyChallenge/script/Solve.s.sol
@@ -9,10 +9,6 @@ contract SolveChallenge is Script {
     address setupAddress = vm.envAddress("SETUP_ADDRESS");
     uint privateKey = vm.envUint("PRIVATE_KEY");
 
-    
-    // The key that we are looking for in the keys mapping
-    uint constant isKey = 0x1337;
-
     function run() public {
         // Start broadcasting with the private key
         vm.startBroadcast(privateKey);
@@ -30,7 +26,7 @@ contract SolveChallenge is Script {
         EasyChallenge challenge = EasyChallenge(address(setup.challengeInstane()));
 
         // Calculate the correct slot for the key in the 'keys' mapping
-        // The mapping 'keys' is at slot 1 (after 'isKey') in the contract, and we are looking for keys[isKey]
+        // The mapping 'keys' is at slot 1 (after 'isKeyFound') in the contract
         bytes32 slot = keccak256(abi.encodePacked(uint(0x1337), uint(1)));
 
         // Now, unlock using the correct slot
@@ -45,6 +41,5 @@ contract SolveChallenge is Script {
 
         vm.stopBroadcast();
     }
-    
 }
 
